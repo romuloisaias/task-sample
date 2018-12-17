638e2d59340887de12581f5c6a90a43fd2fceb34
Lautaro Ezequiel Murua <lautaroemurua@gmail.com>
2018-12-17 14:33:15 -0300

    fix

--- app/controllers/tasksController.js
+++ app/controllers/tasksController.js
@@ -1,7 +1,6 @@
 'use strict';
 var mongoose = require('mongoose'),
 Task = mongoose.model('Tasks');
-
 exports.list_all_tasks = function(req, res) { //listar todos los registros
   Task.find({}, function(err, task) { //aqui find para buscar registro
     if (err){
@@ -15,7 +14,6 @@ exports.list_all_tasks = function(req, res) { //listar todos los registros
     }
   });
 };
-
 exports.create_a_task = function(req, res) {
   var new_task = new Task(req.body);
   new_task.save(function(err, task) { //save para guardar
@@ -31,31 +29,24 @@ exports.create_a_task = function(req, res) {
     }
   });
 };
-
-
 exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) { //para buscar por ID
-    if (err)
-    {
-      res.send(err);
+  Task.findOne({_id:req.params.taskId}, function(err, task) { //para buscar por ID
+    if (err) {
+      return res.status(500).json(err);
     }
     if(task){
-      console.log("found!")
-      res.json(task);
-    }else{
-      res.send("no existe");
-    }  
+      return res.send(task);
+    }
+    return res.sendStatus(404);
   });
 };
-
 exports.create_status = function (req, res){
   var stat = req.body.status.toLowerCase();
   Task.findById(stat, function(err, task) {
 console.log("crea estado");
 res.send(stat);
   })
-}
-
+};
 exports.update_a_task = function(req, res) {
   Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) { //encontrar y actualizar
     if (err)
@@ -69,8 +60,6 @@ exports.update_a_task = function(req, res) {
       }
   });
 };
-
-
 exports.delete_a_task = function(req, res) {
   Task.deleteOne({ //borra registro
     _id: req.params.taskId
@@ -88,4 +77,3 @@ exports.delete_a_task = function(req, res) {
       }
   });
 };
-
