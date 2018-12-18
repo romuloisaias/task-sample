2e254749f00cdf2197089174fbbacee319a4e3e4
Isaias Hernandez <romulohernandez29@gmail.com>
2018-12-18 13:16:49 -0300

    rollback cambios

--- app/controllers/tasksController.js
+++ app/controllers/tasksController.js
@@ -4,11 +4,10 @@ Task = mongoose.model('Tasks');
 exports.list_all_tasks = function(req, res) { //listar todos los registros
   Task.find({}, function(err, task) { //aqui find para buscar registro
     if (err){
-      console.error(err.stack);
       return res.status(500).json(err);
     }
     if(task){
-      console.log(req.method);
+      console.log("list all");
       res.json(task);
     }else{
       res.send("no hay registros!");
@@ -20,7 +19,6 @@ exports.create_a_task = function(req, res) {
   new_task.save(function(err, task) { //save para guardar
     if (err)
     {
-      console.error(err.stack);
       return res.status(500).json(err);
     }
     if(task){
@@ -35,7 +33,6 @@ exports.create_a_task = function(req, res) {
 exports.read_a_task = function(req, res) {
   Task.findOne({_id:req.params.taskId}, function(err, task) { //para buscar por ID
     if (err) {
-      console.error(err.stack);
       return res.status(500).json(err);
     }
     if(task){
@@ -53,7 +50,6 @@ exports.update_status = function (req, res){
         Task.findOneAndUpdate({_id: req.params.taskId}, {status:stat}, {new: true}, function(err, task) {
         if (err)
         {
-          console.error(err.stack);
           return res.status(500).json(err);
         }
           if(task){
@@ -70,7 +66,6 @@ exports.update_a_task = function(req, res) {
   Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) { //encontrar y actualizar
     if (err)
       {
-        console.error(err.stack);
         return res.status(500).json(err);
       }
       if(task)
@@ -85,7 +80,6 @@ exports.delete_a_task = function(req, res) {
   }, function(err, task) {
     if (err)
       {
-        console.error(err.stack);
         res.send(err);
       }
       if(task.n > 0){
