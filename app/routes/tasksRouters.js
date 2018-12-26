08a3e5eb3af708160a0ced67a0505ddae8dc6e74
Lautaro Ezequiel Murua <lautaroemurua@gmail.com>
2018-12-26 10:12:33 -0300

    Fix routes

--- app/routes/tasksRouters.js
+++ app/routes/tasksRouters.js
@@ -3,35 +3,36 @@ module.exports = function(app) {
   //INCLUDES CONTROLLER
   var taskController = require('../controllers/tasksController.js')
   
-  //INCLUDES MIDDLEWARE PROTECTION
+  //INCLUDES MIDDLEWARE AUTHENTICATED
   var md_auth = require('../middlewares/authenticated')
 
   //REDIRECT TO INIT PAGE
-  app.get('/', md_auth.ensureAuth,taskController.initPage)
+  app.get('/',taskController.initPage)
 
-  //LIST ALL ELEMENTS
-  app.get('/listAll', md_auth.ensureAuth,taskController.listAll)
+  //LIST ALL ELEMENTS VIA GET
+  app.get('/listAll',taskController.listAll)
 
   //CREATE NEW TASK VIA POST
-  app.get('/tasks', md_auth.ensureAuth,taskController.createTask)
+  app.post('/tasks',taskController.createTask)
 
   //LIST TASK WITH PAGINATION VIA GET
-  app.get('/tasks/paginator/:page/:elements', md_auth.ensureAuth,taskController.listPages)
+  app.get('/tasks/paginator/:page/:elements',taskController.listPages)
 
-  app.get('/tasks/:id', md_auth.ensureAuth,taskController.updateTask)
+  //UPDATE RECODRS VIA PUT
+  app.put('/tasks/:id',taskController.updateTask)
 
-  app.get('/tasks/:id', md_auth.ensureAuth,taskController.deleteTask)
+  //REMOVE RECODRS VIA DELETE
+  app.delete('/tasks/:id',taskController.deleteTask)
 
   //FIND CONTENT IN TITLE AND LIST TAKS VIA GET
-  app.get('/tasks/title/:title', md_auth.ensureAuth,taskController.searchByTitle)
+  app.get('/tasks/title/:title',taskController.searchByTitle)
 
   //LIST ALL BY ID COLLECTION VIA PUT
-  app.put('/tasks/updateIds/upDateByIds', md_auth.ensureAuth,taskController.updateByIdCollection)
+  app.put('/tasks/updateIds/upDateByIds',taskController.updateByIdCollection)
 
-  app.route('/tasks/updateIds/upDateByIds/')
-    
-  app.get('/tasks/status/:id', md_auth.ensureAuth,taskController.listCollection)
+  //LIST ALL RECORDS WHERE STATUS VIA GET
+  app.get('/tasks/status/:id',taskController.listCollection)
 
-  app.put('/tasks/status/:id', md_auth.ensureAuth,taskController.updateStatus)
-
+  //WILL UPGRADE STATUS, IF NOT EXIST, WILL SUGGESTED AVAILABLE STATUS
+  app.put('/tasks/status/:id',taskController.updateStatus)
 };
