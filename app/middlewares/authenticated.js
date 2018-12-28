33e00df965a396734ed0f60d53452eb21a54a574
mateoscarafia <mateoscarafia@gmail.com>
2018-12-28 17:16:08 -0300

    changes

--- app/middlewares/authenticated.js
+++ app/middlewares/authenticated.js
@@ -4,17 +4,20 @@ var Token = require("../models/tokenModel")
 var jwt = require("jwt-simple")
 var secret = "SECRET_KEY_FOR_MY_TOKEN_BUILDER_26122018"
 
-exports.EnsureAuth = async function(req, res, next) {
+exports.EnsureAuth = async function (req, res, next) {
   if (!req.headers.authorization) {
-    return res.status(401).send({ message: "Send token in header" })
+    return res.status(401).send({
+      message: "Send token in header"
+    })
   }
 
   try {
     var token = req.headers.authorization.replace(/['"]+/g, "")
 
     //CHECK IF TOKEN IS BLOCKED
-    var result = await Token.find({ token: token })
-
+    var result = await Token.find({
+      token: token
+    })
     if (result[0] != null) {
       return res.status(401).send({
         message: "Token blocked"
